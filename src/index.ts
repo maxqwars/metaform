ed84c8d6075837c346e0ce6177a377058b733c4e
Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
2022-02-19 13:16:57 +0700

    update exports

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,9 @@ export { default as UrlTools } from './utils/UrlTools';
 /*                                    Enums                                   */
 /* -------------------------------------------------------------------------- */
 export { default as API_ENDPOINTS } from './enums/API_ENDPOINTS';
+export { default as DESCRIPTION_TYPE } from './enums/DESCRIPTION_TYPE';
+export { default as INCLUDED_RESOURCES } from './enums/INCLUDED_RESOURCES';
+export { default as PLAYLIST_TYPE } from './enums/PLAYLIST_TYPE';
+export { default as TITLE_CONTENT_TYPE } from './enums/TITLE_CONTENT_TYPE';
+export { default as TITLE_SEASON } from './enums/TITLE_SEASON';
+export { default as WEEK_DAY } from './enums/WEEK_DAY';
