884b21169389f3d79726681bf6601687306e3bf8
Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
2022-02-19 13:09:06 +0700

    added: new enums -> DESCRIPTION_TYPE, INCLUDED_RESURCES, PLAYLIST_TYPE

---
+++ src/enums/PLAYLIST_TYPE.ts
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-shadow
+enum PLAYLIST_TYPE {
+  ARRAY = 'array',
+  OBJECT = 'object',
+}
+
+export default PLAYLIST_TYPE;
