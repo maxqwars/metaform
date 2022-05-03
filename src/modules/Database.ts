26a0910ec9175c7510a112b870a1390c054a127d
Maxim "maxqwars" Maximenko <maxqwars@gmail.com>
2022-05-03 15:16:33 +0700

    changed: Database module, add getRandomTitle method

--- src/modules/Database.ts
+++ src/modules/Database.ts
@@ -5,7 +5,7 @@
 
 import API_VERSION from '../constants/API_VERSION';
 import API_ENDPOINT from '../constants/API_ENDPOINTS';
-import * as DatabaseTypes from '../typings/Title';
+import { Title } from '../typings/Title';
 import GetTitleQueryBuilder from '../classes/GetTitleQueryBuilder';
 import RequestUrlBuilder from '../core/RequestURLBuilder';
 import CoreModule from './CoreModule';
@@ -29,9 +29,51 @@ export default class Database extends CoreModule {
     this.options = options;
   }
 
+  async getRandomTitle(
+    params: IGetTitleQueryParams
+  ): Promise<Title | APIError | Error> {
+    let apiResponse: Object;
+
+    const Q_BUILD = new GetTitleQueryBuilder();
+
+    const U_BUILD = new RequestUrlBuilder(
+      this.options.baseUrl,
+      this.options.version,
+      this.options.useHttps
+    );
+
+    U_BUILD.setEndpoint(API_ENDPOINT.GET_RANDOM_TITLE);
+
+    Q_BUILD.setId(params.id)
+      .setCode(params.code)
+      .setFilter(params.filter)
+      .setRemove(params.remove)
+      .setInclude(params.include)
+      .setTorrentId(params.torrentId)
+      .setPlaylistType(params.playlistType)
+      .setDescriptionType(params.descriptionType);
+
+    U_BUILD.setQueryParams(Q_BUILD.build());
+
+    const FINAL_URL = U_BUILD.build();
+
+    try {
+      apiResponse = await (
+        await this.fetchWithTimeout(FINAL_URL, this.options.timeout)
+      ).json();
+    } catch (e) {
+      throw new Error('Unexpected error');
+    }
+
+    // eslint-disable-next-line no-prototype-builtins
+    return apiResponse.hasOwnProperty('error')
+      ? (apiResponse as APIError)
+      : titleParser(apiResponse as RawTitle);
+  }
+
   async getTitle(
     params: IGetTitleQueryParams
-  ): Promise<DatabaseTypes.Title | APIError | Error> {
+  ): Promise<Title | APIError | Error> {
     let apiResponse: Object;
 
     const Q_BUILD = new GetTitleQueryBuilder();
