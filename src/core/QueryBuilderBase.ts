3f9b34224f260d817817f051d2058b94e9fe626a
Maxim Maximenko <maxqwars@gmail.com>
2022-06-26 01:21:02 +0700

    added: tests for QueryBuilderBase

--- src/core/QueryBuilderBase.ts
+++ src/core/QueryBuilderBase.ts
@@ -3,16 +3,53 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-underscore-dangle */
+
 export default class QueryBuilderBase {
-  protected params: { [key: string]: unknown } = {};
+  private _params: { [key: string]: unknown } = {};
+
+  protected get params(): { [key: string]: unknown } {
+    return this._params;
+  }
+
+  protected set params(value: { [key: string]: unknown }) {
+    this._params = value;
+  }
 
   protected addParam(key: string, value: unknown): void {
-    this.params[key] = value;
+    this._params[key] = value;
   }
 
   protected build(): string {
     return Object.keys(this.params)
       .map(key => `${key}=${this.params[key]}`)
       .join('&');
+  }
+
+  protected isArray(arr: any): boolean {
+    if (Array.isArray(arr)) {
+      const obj = arr as Object;
+      return Array.isArray(arr) && obj.hasOwnProperty('length');
+    }
+    return false;
+  }
+
+  protected isString(str: any): boolean {
+    return typeof str === 'string' || str instanceof String;
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  protected isObj(obj: any): boolean {
+    return false;
+  }
+
+  protected isNumber(num: any): boolean {
+    return Number.isInteger(num) || Number.isFinite(num);
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  protected isBool(bool: any): boolean {
+    return false;
   }
 }
