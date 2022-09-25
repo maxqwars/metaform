12634cc5f72d9beec612c097fd150423dd5dbdb8
Maxim Maximenko <maxqwars@gmail.com>
2022-09-25 16:32:52 +0700

    added: Implement getChanged, getRandomRelease, getYearsList, getGenresList

--- src/modules/MetaDatabase.ts
+++ src/modules/MetaDatabase.ts
@@ -1,28 +1,78 @@
 import { MetaModule } from "../core";
+import { MetaModuleOptions, MetaModuleResponse, Title } from "../types";
+import { API_METHOD, MOD_ERR } from "../enums";
 
 export class MetaDatabase extends MetaModule {
-  get() {
+  constructor(options?: MetaModuleOptions) {
+    super(options);
+  }
+
+  async getTitle() {
     throw Error("Not implemented");
   }
 
-  getFromList() {
+  async getFromList() {
     throw Error("Not implemented");
   }
 
-  getChanged() {
-    throw Error("Not implemented");
+  async getChanged(): Promise<MetaModuleResponse<Title[] | null>> {
+    const requestUrl = this._urlBuilder
+      .useMethod(API_METHOD.GET_CHANGES)
+      .useQuery("")
+      .build();
+
+    try {
+      const response = await this._fetchWithTimeout(requestUrl, {});
+      return this._makeResponse<Title[]>(false, await response.json());
+    } catch (e) {
+      return this._makeResponse<null>(true, null, MOD_ERR.UNKNOWN_ERROR);
+    }
   }
 
-  getRandomRelease() {
-    throw Error("Not implemented");
+  async getRandomRelease(): Promise<MetaModuleResponse<Title | null>> {
+    const requestUrl = this._urlBuilder
+      .useMethod(API_METHOD.GET_RANDOM_TITLE)
+      .useQuery("")
+      .build();
+
+    try {
+      const response = await this._fetchWithTimeout(requestUrl, {});
+      return this._makeResponse<Title>(false, await response.json());
+    } catch (e) {
+      return this._makeResponse<null>(true, null, MOD_ERR.UNKNOWN_ERROR);
+    }
   }
 
-  getYearsList() {
-    throw Error("Not implemented");
+  async getYearsList(): Promise<MetaModuleResponse<number[] | null>> {
+    const requestUrl = this._urlBuilder
+      .useMethod(API_METHOD.GET_YEARS)
+      .useQuery("")
+      .build();
+
+    try {
+      const response = await this._fetchWithTimeout(requestUrl, {
+        timeout: this._options.timeout,
+      });
+      return this._makeResponse<number[]>(false, await response.json());
+    } catch (e) {
+      return this._makeResponse(true, null, MOD_ERR.UNKNOWN_ERROR);
+    }
   }
 
-  getGenresList() {
-    throw Error("Not implemented");
+  async getGenresList(): Promise<MetaModuleResponse<string[] | null>> {
+    const requestUrl = this._urlBuilder
+      .useMethod(API_METHOD.GET_GENRES)
+      .useQuery("")
+      .build();
+
+    try {
+      const response = await this._fetchWithTimeout(requestUrl, {
+        timeout: this._options.timeout,
+      });
+      return this._makeResponse<string[]>(false, await response.json());
+    } catch (e) {
+      return this._makeResponse<null>(true, null, MOD_ERR.UNKNOWN_ERROR);
+    }
   }
 
   find() {
