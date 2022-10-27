6adcd7c2e7ccba8d142341374fc48436d63b490e
Maxim Maximenko <maxqwars@gmail.com>
2022-10-27 20:19:43 +0700

    added: MetaAbout.getTeam()

--- src/modules/MetaAbout.ts
+++ src/modules/MetaAbout.ts
@@ -1,7 +1,22 @@
 import { MetaModule } from "../core";
+import { MetaModuleOptions, MetaModuleResponse, TeamInfo } from "../types";
+import { API_METHOD, MOD_ERR } from "../enums";
 
 export class MetaAbout extends MetaModule {
-  getTeam() {
-    throw Error("Not implemented");
+  constructor(options?: MetaModuleOptions) {
+    if (options) super(options);
+    super();
+  }
+
+  async getTeam(): Promise<MetaModuleResponse<TeamInfo | null>> {
+    const requestUrl = this._urlBuilder.useMethod(API_METHOD.GET_TEAM).build();
+
+    try {
+      const response = await this._fetchWithTimeout(requestUrl, {});
+      const data = await response.json();
+      return this._makeResponse<TeamInfo>(false, data);
+    } catch (e) {
+      return this._makeResponse<null>(true, null, MOD_ERR.UNKNOWN_ERROR);
+    }
   }
 }
