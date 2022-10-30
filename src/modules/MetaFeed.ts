d3b1960f239eeaed33f7b0cb3ae3f1f5ada15dee
Maxim Maximenko <maxqwars@gmail.com>
2022-10-30 20:50:50 +0700

    implement: MetaFeed.getSchedule()

--- src/modules/MetaFeed.ts
+++ src/modules/MetaFeed.ts
@@ -1,8 +1,58 @@
 import { MetaModule } from "../core";
+import {
+  MetaModuleOptions,
+  MetaModuleResponse,
+  GetScheduleQueryParams,
+  ScheduleItem,
+} from "../types";
+import { API_METHOD, MOD_ERR } from "../enums";
+import { SamplingQueryBuilder, FormatQueryBuilder } from "../utils";
 
 export class MetaFeed extends MetaModule {
-  getSchedule() {
-    throw Error("Not implemented");
+  constructor(options?: MetaModuleOptions) {
+    if (options) super(options);
+    super();
+  }
+
+  async getSchedule(
+    params?: GetScheduleQueryParams
+  ): Promise<MetaModuleResponse<ScheduleItem[] | null>> {
+    // Init query builders
+    const samplingQB = new SamplingQueryBuilder();
+    const formatQB = new FormatQueryBuilder();
+
+    const samplingQueryString = samplingQB
+      .filter(params?.filter)
+      .include(params?.include)
+      .remove(params?.remove)
+      .build();
+
+    const formatQueryString = formatQB
+      .descriptionFormat(params?.descriptionFormat)
+      .playlistFormat(params?.playlistFormat)
+      .build();
+
+    let finalQueryString = "";
+
+    // Concat query params
+    if (samplingQueryString) finalQueryString += samplingQueryString;
+    if (formatQueryString) finalQueryString += `&${formatQueryString}`;
+    if (params?.days) finalQueryString += `&days=${params.days.join(",")}`;
+
+    // Construct API request URL
+    const apiRequestUrl = this._urlBuilder
+      .useMethod(API_METHOD.GET_SCHEDULE)
+      .useQuery(finalQueryString)
+      .build();
+
+    try {
+      const data = await (
+        await this._fetchWithTimeout(apiRequestUrl, {})
+      ).json();
+      return this._makeResponse<ScheduleItem[]>(false, data);
+    } catch (e) {
+      return this._makeResponse<null>(true, null, MOD_ERR.UNKNOWN_ERROR);
+    }
   }
 
   getYouTube() {
