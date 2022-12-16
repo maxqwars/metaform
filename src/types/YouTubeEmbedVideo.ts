6045ba6c6660e61aa1c18e1dbb4743d3ea880ae7
Maxim Maximenko <maxqwars@gmail.com>
2022-12-16 13:05:39 +0700

    changed: YouTubeEmbedVideo (getYouTube)

--- src/types/YouTubeEmbedVideo.ts
+++ src/types/YouTubeEmbedVideo.ts
@@ -2,6 +2,12 @@ export type YouTubeEmbedVideo = {
   id?: number;
   title?: string;
   image?: string;
+  preview?: {
+    src?: string;
+    thumbnail?: string;
+  };
   youtubeId?: string;
+  comments?: number;
+  views?: number;
   timestamp?: number;
 };
