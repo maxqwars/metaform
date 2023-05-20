91e585c4526e121d37129e4419ff9838ba1cee1a
Maxim Maximenko <maxqwars@gmail.com>
2023-05-20 15:57:01 +0700

    install new development dependencies, change rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,65 @@
 import typescriptPlugin from "rollup-plugin-typescript2";
 import envFiles from "@jjldxz/rollup-plugin-env-files";
+import multiInput from "rollup-plugin-multi-input";
+import terser from "@rollup/plugin-terser";
 
-const dist = "dist";
+const rootBuildDir = "dist";
+const rootSourceDir = "src";
 
 export default {
-  input: "./src/index.ts",
+  input: [`${rootSourceDir}/metaform2/*.ts`, `${rootSourceDir}/metaform3/*.ts`],
   plugins: [
+    multiInput({ relative: "src/" }),
     typescriptPlugin(),
-    {
-      name: "retain-import-expression",
-      resolveDynamicImport(specifier) {
-        if (specifier === "node-fetch") return false;
-        return null;
-      },
-      renderDynamicImport({ targetModuleId }) {
-        if (targetModuleId === "node-fetch") {
-          return {
-            left: "import(",
-            right: ")",
-          };
-        }
-
-        return undefined;
-      },
-    },
-    envFiles({
-      preventAssignment: true,
-    }),
+    envFiles({ preventAssignment: true }),
+    terser(),
   ],
-  output: [
-    {
-      file: `${dist}/index.js`,
-      format: "cjs",
-    },
-    {
-      file: `${dist}/index.mjs`,
-      format: "esm",
-    },
-    {
-      name: "metaform",
-      file: `${dist}/index.umd.js`,
-      format: "umd",
-    },
-  ],
+  output: {
+    format: "esm",
+    dir: rootBuildDir,
+  },
 };
+
+// const dist = "dist";
+
+// export default {
+//   input: "./src/index.ts",
+//   plugins: [
+//     typescriptPlugin(),
+//     {
+//       name: "retain-import-expression",
+//       resolveDynamicImport(specifier) {
+//         if (specifier === "node-fetch") return false;
+//         return null;
+//       },
+//       renderDynamicImport({ targetModuleId }) {
+//         if (targetModuleId === "node-fetch") {
+//           return {
+//             left: "import(",
+//             right: ")",
+//           };
+//         }
+
+//         return undefined;
+//       },
+//     },
+//     envFiles({
+//       preventAssignment: true,
+//     }),
+//   ],
+//   output: [
+//     {
+//       file: `${dist}/index.js`,
+//       format: "cjs",
+//     },
+//     {
+//       file: `${dist}/index.mjs`,
+//       format: "esm",
+//     },
+//     {
+//       name: "metaform",
+//       file: `${dist}/index.umd.js`,
+//       format: "umd",
+//     },
+//   ],
+// };
