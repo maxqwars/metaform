aa64804ad5031e89d3ec841df9027d21718f3a91
Maxim Maximenko <maxqwars@gmail.com>
2023-08-22 13:09:15 +0700

    Added: New exports (Metaform3)

---
+++ src/metaform3/index.ts
@@ -0,0 +1,13 @@
+/* Classes */
+export { Metaform3 } from "./metaform3";
+export { URLConstructor } from "./core/URLConstructor";
+
+/* Enums */
+export { API_METHOD_PATH, METAFORM_ERROR, REL_SEASON, WEEK_DAY } from "./enums";
+
+/* Functions */
+export { extractDomainFromUrl } from "./utils/extractDomainFromUrl";
+export { object2query } from "./utils/object2query";
+
+/* Types */
+export type { Params, Objects, Responses } from "./schemas";
