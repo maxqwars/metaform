46b95e5db454aa961cf88d8d324cfbcc5d479fb9
Maxim Maximenko <maxqwars@gmail.com>
2026-05-20 20:40:36 +0700

    Added vitest to project

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,7 @@ export default defineConfig([
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
+        allowDefaultProject: ['vitest.config.ts', 'tsdown.config.ts'],
       },
     },
   },
