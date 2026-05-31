06c2e35ae4081bb1a4e3590b330f026c9b8c4ab0
Maxim Maximenko <maxqwars@gmail.com>
2026-05-31 21:29:07 +0700

    The build configuration has been updated, and support for building a standalone bundle has been added

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,16 +1,29 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig({
-  tsconfig: 'tsconfig.lib.json',
-  entry: {
-    index: 'src/index.ts',
-    'scheme/v1': 'src/scheme/v1/index.ts',
-    transport: 'src/transport/index.ts',
+export default defineConfig([
+  {
+    tsconfig: 'tsconfig.lib.json',
+    entry: {
+      index: 'src/index.ts',
+      'scheme/v1': 'src/scheme/v1/index.ts',
+      transport: 'src/transport/index.ts',
+    },
+    outDir: 'dist',
+    format: ['esm', 'cjs'],
+    dts: true,
+    clean: true,
+    sourcemap: true,
+    unbundle: true,
   },
-  outDir: 'dist',
-  format: ['esm', 'cjs'],
-  dts: true,
-  clean: true,
-  sourcemap: true,
-  unbundle: true,
-})
+  {
+    tsconfig: 'tsconfig.lib.json',
+    entry: {
+      metaform: 'src/bundle.ts',
+    },
+    outDir: 'dist-bundle',
+    format: ['esm'],
+    dts: true,
+    clean: true,
+    sourcemap: false,
+  },
+])
