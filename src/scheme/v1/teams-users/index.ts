a9a61f1ab5b51a5d9685ce4531f954f8675fa331
Maxim Maximenko <maxqwars@gmail.com>
2026-06-09 18:13:44 +0700

    Implement mapper for teams-users. Upd. exports file

--- src/scheme/v1/teams-users/index.ts
+++ src/scheme/v1/teams-users/index.ts
@@ -1,1 +1,4 @@
 export type * from './types'
+export * from './guards'
+export * from './mapper'
+export * from './serialize-params'
---
+++ src/scheme/v1/teams-users/mapper.ts
@@ -0,0 +1,46 @@
+import { mapTeamsDto } from '../teams'
+import { mapTeamsRolesDto } from '../teams-roles'
+import type {
+  TeamsUsers,
+  TeamsUsersDto,
+  TeamsUsersUser,
+  TeamsUsersUserDto,
+  TeamsUsersUserAvatarImage,
+  TeamsUsersUserAvatarImageDto,
+} from './types'
+
+function mapTeamsUsersUserAvatarImageDto(
+  dto: TeamsUsersUserAvatarImageDto,
+): TeamsUsersUserAvatarImage {
+  return {
+    preview: dto.preview ?? null,
+    thumbnail: dto.thumbnail ?? null,
+    optimized: dto.optimized
+      ? {
+          preview: dto.optimized.preview ?? null,
+          thumbnail: dto.optimized.thumbnail ?? null,
+        }
+      : undefined,
+  }
+}
+
+function mapTeamsUsersUserDto(dto: TeamsUsersUserDto): TeamsUsersUser {
+  return {
+    id: dto.id ?? null,
+    nickname: dto.nickname ?? null,
+    avatar: dto.avatar ? mapTeamsUsersUserAvatarImageDto(dto.avatar) : null,
+  }
+}
+
+export function mapTeamsUsersDto(dto: TeamsUsersDto): TeamsUsers {
+  return dto.map((item) => ({
+    id: item.id ?? null,
+    nickname: item.nickname ?? null,
+    isIntern: item.is_intern ?? null,
+    sortOrder: item.sort_order ?? null,
+    isVacation: item.is_vacation ?? null,
+    user: item.user ? mapTeamsUsersUserDto(item.user) : null,
+    roles: item.roles ? mapTeamsRolesDto(item.roles) : null,
+    teams: item.teams ? mapTeamsDto(item.teams) : null,
+  }))
+}
