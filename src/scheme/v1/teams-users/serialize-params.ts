d81c99ae7bf3643b8b060958ead82e876b2dd3a6
Maxim Maximenko <maxqwars@gmail.com>
2026-06-13 18:49:18 +0700

    Fix copy-paste namin in teams-users params serializer

--- src/scheme/v1/teams-users/serialize-params.ts
+++ src/scheme/v1/teams-users/serialize-params.ts
@@ -1,6 +1,6 @@
 import type { TeamsUsersParams } from './types'
 
-export function serializeTeamsRolesParams(params: TeamsUsersParams): Record<string, string> {
+export function serializeTeamsUsersParams(params: TeamsUsersParams): Record<string, string> {
   return {
     ...(params.include && { include: params.include.join(',') }),
     ...(params.exclude && { exclude: params.exclude.join(',') }),
