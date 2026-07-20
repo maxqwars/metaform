e6b70964edaabaa9f03f25128d3676fb33d77b56
Maxim Maximenko <maxqwars@gmail.com>
2026-07-21 00:27:19 +0700

    refactor(shared): add type guards for image

--- src/scheme/v1/shared/image/guards.ts
+++ src/scheme/v1/shared/image/guards.ts
@@ -1,6 +1,22 @@
-import type { ImageDto } from './types'
+import type { ImageDto, ImageOptimizedDto } from './types'
+import { isOptionalString, isRecord } from '../../../../helpers/type-helpers'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function isImageDto(dto: ImageDto): boolean {
-  throw Error('Not implemented')
+export function isImageDto(value: unknown): value is ImageDto {
+  if (!isRecord(value)) return false
+
+  const checks = [
+    isOptionalString(value.preview),
+    isOptionalString(value.thumbnail),
+    value.optimized === undefined || isImageOptimizedDto(value.optimized),
+  ]
+
+  return checks.every(Boolean)
+}
+
+export function isImageOptimizedDto(value: unknown): value is ImageOptimizedDto {
+  if (!isRecord(value)) return false
+
+  const checks = [isOptionalString(value.preview), isOptionalString(value.thumbnail)]
+
+  return checks.every(Boolean)
 }
