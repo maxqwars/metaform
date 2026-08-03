888b7047706f2aae07847b2360aaaa32b38bce7e
Maxim Maximenko <maxqwars@gmail.com>
2026-08-03 19:24:14 +0700

    feat(api): implement mapMediaPromotionsDto

--- src/scheme/v1/media/promotions/mapper.ts
+++ src/scheme/v1/media/promotions/mapper.ts
@@ -1,6 +1,19 @@
 import type { MediaPromotionsDto, MediaPromotions } from './types'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ImageMappers } from '../../shared/image'
+import { ReleaseMappers } from '../../shared/release'
+
 export function mapMediaPromotionsDto(dto: MediaPromotionsDto): MediaPromotions {
-  throw Error('Not implemented')
+  return dto.map((item) => ({
+    id: item.id ?? null,
+    url: item.url ?? null,
+    image: item.image ? ImageMappers.mapImageDto(item.image) : null,
+    title: item.title ?? null,
+    isAd: item.is_ad ?? null,
+    adErid: item.ad_erid ?? null,
+    adOrigin: item.ad_origin ?? null,
+    urlLabel: item.url_label ?? null,
+    hasOverlay: item.has_overlay ?? null,
+    release: item.release ? ReleaseMappers.mapReleaseDto(item.release) : null,
+  }))
 }
