e17246979d83999596619b7cb37053d455c31e96
Maxim Maximenko <maxqwars@gmail.com>
2026-08-20 21:43:03 +0700

    fix(helpers): update NestedKeyOf to match documentation

--- src/helpers/nestedKeyOf.ts
+++ src/helpers/nestedKeyOf.ts
@@ -1,7 +1,9 @@
 export type NestedKeyOf<T> = T extends object
   ? {
-      [K in keyof T & (string | number)]: T[K] extends object
-        ? `${K}` | `${K}.${NestedKeyOf<T[K]>}`
+      [K in keyof T & (string | number)]: NonNullable<T[K]> extends object
+        ? NonNullable<T[K]> extends readonly unknown[]
+          ? `${K}`
+          : `${K}` | `${K}.${NestedKeyOf<NonNullable<T[K]>>}`
         : `${K}`
     }[keyof T & (string | number)]
   : never
