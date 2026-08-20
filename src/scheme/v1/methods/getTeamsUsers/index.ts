42ee51525eb2cb8ebf5a56feeb670cda0d83b89b
Maxim Maximenko <maxqwars@gmail.com>
2026-08-20 21:46:55 +0700

    fix(api): correct getteamsusers schema

--- src/scheme/v1/methods/getTeamsUsers/index.ts
+++ src/scheme/v1/methods/getTeamsUsers/index.ts
@@ -1,28 +1,29 @@
-import { Team, User, Role } from '@/scheme/v1'
+import { Team, User, Role, Account } from '@/scheme/v1'
+import type { components } from '@/generated/scheme.v1'
+import type { NestedKeyOf } from '@/helpers/nestedKeyOf'
 
-export interface GetTeamsUsersApiResponseItem {
-  user: User.scheme.UserApiResponse | null
-  team: Team.scheme.TeamApiResponse
-  roles: Role.scheme.RoleApiResponse[]
-}
-
+/*
+ * ⚠️ Fix for the OpenAPI schema: the user field of the responses.api.v1.teams.users object can be null, which was not defined in the schema.
+ */
+export type GetTeamsUsersApiResponseItem =
+  components['schemas']['responses.api.v1.teams.users'][number] & {
+    user?: components['schemas']['models.teams.v1.team.user.account'] | null
+  }
 export type GetTeamsUsersApiResponse = GetTeamsUsersApiResponseItem[]
 
-export interface GetTeamsUsersResponseItem {
-  user: User.scheme.User | null
+export interface GetTeamsUsersResponseItem extends User.scheme.User {
+  user: Account.scheme.Account | null
   team: Team.scheme.Team | null
   roles: Role.scheme.Role[] | null
 }
 
 export type GetTeamsUsersResponse = GetTeamsUsersResponseItem[]
 
+export type GetTeamsUsersFields = NestedKeyOf<GetTeamsUsersApiResponseItem>
+
 export interface GetTeamsUsersQueryParams {
-  include?: (Role.scheme.RoleFieldsPaths &
-    Team.scheme.TeamFieldsPaths &
-    User.scheme.UserFieldsPaths)[]
-  exclude?: (Role.scheme.RoleFieldsPaths &
-    Team.scheme.TeamFieldsPaths &
-    User.scheme.UserFieldsPaths)[]
+  include?: GetTeamsUsersFields[]
+  exclude?: GetTeamsUsersFields[]
 }
 
 export function serializeGetTeamsUsersQueryParams(
@@ -35,23 +36,27 @@ export function serializeGetTeamsUsersQueryParams(
 }
 
 export function isGetTeamsUsersApiResponse(value: unknown): value is GetTeamsUsersApiResponse {
-  return (
-    Array.isArray(value) &&
-    value.every((item: GetTeamsUsersApiResponseItem): boolean => {
-      return (
-        Team.guards.isOptionalTeamApiResponse(item.team) &&
-        User.guards.isNullableOptionalUserApiResponse(item.user) &&
-        item.roles.every((role) => Role.guards.isOptionalRoleResponse(role))
-      )
-    })
-  )
+  if (!Array.isArray(value)) return false
+
+  return value.every((item: GetTeamsUsersApiResponseItem): boolean => {
+    return (
+      Team.guards.isOptionalTeamApiResponse(item.team) &&
+      Account.guards.isNullableOptionalAccountApiResponse(item.user) &&
+      (typeof item.roles !== 'undefined'
+        ? item.roles.every((role) => Role.guards.isOptionalRoleResponse(role))
+        : true)
+    )
+  })
 }
 
 export function toGetTeamsUsersResponseItem(
   dto: GetTeamsUsersApiResponseItem,
 ): GetTeamsUsersResponseItem {
+  const UserFields = User.mappers.toUser(dto)
+
   return {
-    user: dto.user === null ? null : User.mappers.toUser(dto.user),
+    ...UserFields,
+    user: !dto.user ? null : Account.mappers.toAccount(dto.user),
     team: typeof dto.team !== 'undefined' ? Team.mappers.toTeam(dto.team) : null,
     roles:
       typeof dto.roles !== 'undefined' ? dto.roles.map((item) => Role.mappers.toRole(item)) : null,
