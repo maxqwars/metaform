c805418328b53949444f2f8c044dd674a75987e9
Maxim Maximenko <maxqwars@gmail.com>
2026-09-21 13:08:36 +0700

    feat(video): add type guard stubs for video schema types

---
+++ src/scheme/v1/video/guards.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {} from '@/helpers/type-guards'
+import type {
+  VideoContentApiResponse,
+  VideoOriginApiResponse,
+  VideoOriginTypeApiResponse,
+  VideoOriginTypeEnumApiResponse,
+} from './types'
+
+// TODO: Implement isVideoContentApiResponse typeguard
+export function isVideoContentApiResponse(value: unknown): value is VideoContentApiResponse {
+  throw Error('Not implemented')
+}
+
+// TODO: Implement isVideoOriginApiResponse typeguard
+export function isVideoOriginApiResponse(value: unknown): value is VideoOriginApiResponse {
+  throw Error('Not implemented')
+}
+
+// TODO: Implement isVideosOriginTypeApiResponse typeguard
+export function isVideosOriginTypeApiResponse(value: unknown): value is VideoOriginTypeApiResponse {
+  throw Error('Not implemented')
+}
+
+// TODO: Implement isVideoOriginTypeEnumApiResponse typeguard
+export function isVideoOriginTypeEnumApiResponse(
+  value: unknown,
+): value is VideoOriginTypeEnumApiResponse {
+  throw Error('Not implemented')
+}
